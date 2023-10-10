002ef544c286b6f80475e109d2ac1645a0991a95
David M <136613722+realize-1337@users.noreply.github.com>
2023-10-10 20:57:54 +0200

    working

--- Calculator.py
+++ Calculator.py
@@ -1,34 +1,49 @@
 import math
 import sympy as sp
 
+class Calculator():
+    def __init__(self, targetTemp, targetViscosity) -> None:
+        self.c = sp.Symbol('c')
+        self.a = sp.Symbol('a')
+        self.T = targetTemp
+        self.my = targetViscosity
+        
+        
 
-def createEq(a, b, alpha):
-    eq = sp.Eq(alpha, 1-c+(a*b*c*(1-c))/(a*c+b*(1-c)))
-    return eq
+    def createEq(self, a, b, alpha):
+        eq = sp.Eq(alpha, 1-self.c+(a*b*self.c*(1-self.c))/(a*self.c+b*(1-self.c)))
+        return eq
 
-def createViscosity(my, w, g):
-    eq = sp.Eq(my, w**a*g**(1-a))
-    return eq
+    def createViscosity(self, w, g):
+        eq = sp.Eq(self.my, w**self.a*g**(1-self.a))
+        return eq
 
-def calcMy_g(T):
-    return 12100*math.exp((-1233+T)*T/(9900+70*T))
+    def calcMy_g(self):
+        return 12100*math.exp((-1233+self.T)*self.T/(9900+70*self.T))
 
-def calcMy_w(T):
-    return 1.790*math.exp((-1230-T)*T/(36100+360*T))
+    def calcMy_w(self):
+        return 1.790*math.exp((-1230-self.T)*self.T/(36100+360*self.T))
+
+    def calcAB(self):
+        a = 0.705 - 0.0017*self.T
+        b = (4.9 + 0.036*self.T)*a**2.5
+        return [a,b]
+    
+    def solve(self):
+        eq_alpha = self.createViscosity(self.calcMy_w(), self.calcMy_g())
+        alpha = sp.solve(eq_alpha, self.a)[0]
+        eq = self.createEq(self.calcAB()[0], self.calcAB()[1], alpha)
+        results = [x for x in sp.solve(eq, self.c) if x <=1 and x >= 0]
+        return results[0]
+
 
 if __name__ == '__main__':
-    c = sp.Symbol('c')
-    a = sp.Symbol('a')
     targetViscosity = float(input('Ziel-Viskosität in mPa s angeben: '))
     T = float(input('Aktuelle Umgebungtemperatur in °C angeben: '))
 
     print(f'Ziel-Viskosität {targetViscosity} mPa s bei {T} °C')
     
-    eq_alpha = createViscosity(targetViscosity, calcMy_w(T), calcMy_g(T))
-
-    alpha = sp.solve(eq_alpha, a)[0]
-    eq = createEq(0.671, 2.072, alpha)
-
-    results = [x for x in sp.solve(eq,c) if x <=1 and x >= 0]
-    print(f'Bei {T} °C werden für eine Zielviskosität von {targetViscosity} mPa s {"%.2f" % (results[0]*100)} wt-% Glycerin benötigt!')
+    calc = Calculator(T, targetViscosity)
+    result = calc.solve()
+    print(f'Bei {T} °C werden für eine Zielviskosität von {targetViscosity} mPa s {"%.2f" % (result*100)} wt-% Glycerin benötigt!')
     input('Bitte Enter zum beednden...')
---
+++ ViscoCalc_GUI.py
@@ -0,0 +1,49 @@
+import sys
+import typing
+from PyQt6 import QtCore, QtGui
+from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox
+from GUI.GUI_Code import Ui_ViscoCalc
+import Calculator as ca
+import math
+import sympy as sp
+
+# pyuic6.exe -o .\GUI\GUI_Code.py .\GUI\ViscoCalc.ui
+
+        
+class GUI(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_ViscoCalc()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.calculator)
+        self.ui.toolButton.clicked.connect(self.infobox)
+        # self.ui.setWindowIcon('VC_Logo.ico')
+        self.show()
+
+    def calculator(self):
+        self.ui.label_2.setText('Berechnung läuft')
+        my_target = self.ui.input_my.value()
+        temp = self.ui.input_T.value()
+        print(my_target, temp)
+
+        calc = ca.Calculator(temp, my_target)
+        result = "%.2f" % (calc.solve()*100)
+        print(result)
+        output = f'Für eine Viskosität von {"%.0f" % my_target} mPa s bei {temp} °C <br> werden <b>{result} wt-% </b> Glycerin benötigt.'
+        self.ui.label_2.setText(output)
+
+    def infobox(self):
+        newbox = QMessageBox()
+        newbox.setText('<br> <b> Viskosität Berechnung für Glycerin-Wasser-Mischungen zwischen 0 und 100 °C </b><br>Basierend auf Cheng, N.-S. (2008). "Formula for the viscosity of a glycerol− water mixture." Industrial & Engineering Chemistry Research 47(9): 3285-3288. <br><br> © David Märker, 2023')
+        newbox.setWindowTitle('Infos')
+        newbox.setTextFormat(QtCore.Qt.TextFormat.RichText)
+        newbox.setWindowIcon(QtGui.QIcon('assets/VC_Logo.ico'))
+        newbox.exec()
+        
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GUI()
+    sys.exit(app.exec())
